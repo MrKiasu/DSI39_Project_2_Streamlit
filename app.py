8613409623e638647bbfaf7c7e7b5b144a677131
mrkiasu <jackster.seah@gmail.com>
2023-09-14 23:16:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def main():
     style = """<div style='background-color:black; padding:12px'>
-              <h1 style='color:white'>What is the estimated resale price?</h1>
+              <h1 style='color:white'>What is the estimated HDB resale price?</h1>
        </div>"""
     
     st.markdown(style, unsafe_allow_html=True)
